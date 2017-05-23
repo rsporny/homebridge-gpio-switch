257a4f888cd171d5b7a4409047f06ef9cca178f8
Radoslaw Sporny <r.sporny@gmail.com>
2017-05-23 15:20:19 +0200

    first release

---
+++ index.js
@@ -0,0 +1,44 @@
+var rpio = require("rpio");
+var Service, Characteristic;
+
+module.exports = function(homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+
+  homebridge.registerAccessory("homebridge-gpio-switch", "Switch", SwitchAccessory);
+}
+
+function SwitchAccessory(log, config) {
+  this.log = log;
+  this.name = config['name'];
+  this.pin = config['pin'];
+
+  this.service = new Service.Switch(this.name);
+
+  this.infoService = new Service.AccessoryInformation();
+  this.infoService
+    .setCharacteristic(Characteristic.Manufacturer, "Radoslaw Sporny")
+    .setCharacteristic(Characteristic.Model, "RaspberryPi GPIO Switch")
+    .setCharacteristic(Characteristic.SerialNumber, "Version 1.0.0");
+
+  // use gpio pin numbering
+  rpio.init({
+    mapping: 'gpio'
+  });
+  rpio.open(this.pin, rpio.OUTPUT, rpio.HIGH);
+
+  this.service
+    .getCharacteristic(Characteristic.On)
+    .on('set', this.setPowerState.bind(this));
+}
+
+SwitchAccessory.prototype.setPowerState = function(value, callback) {
+  this.log("Setting switch to %s", value);
+  rpio.write(this.pin, (value ? rpio.LOW : rpio.HIGH));
+  callback();
+}
+
+SwitchAccessory.prototype.getServices = function() {
+  return [this.infoService, this.service];
+}
+
