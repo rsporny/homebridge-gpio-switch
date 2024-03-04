d739c364a1922308e1fee739f5597ba88ac66feb
Nick Sayer <nsayer@users.noreply.github.com>
2024-03-03 17:31:44 -0800

    Redo the patch using the asynchronous calls in node-persist

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+cat index.js
 var rpio = require("rpio");
 var Service, Characteristic, User;
 var storage = require("node-persist");
@@ -18,7 +19,6 @@ function SwitchAccessory(log, config) {
   this.invert = config['invert'];
   this.storage = storage.create();
   const cacheDir = User.persistPath();
-  this.storage.initSync({dir: cacheDir, forgiveParseErrors: true});
 
   this.service = new Service.Switch(this.name);
 
@@ -27,28 +27,30 @@ function SwitchAccessory(log, config) {
     .setCharacteristic(Characteristic.Manufacturer, "Radoslaw Sporny")
     .setCharacteristic(Characteristic.Model, "RaspberryPi GPIO Switch")
     .setCharacteristic(Characteristic.SerialNumber, "Version 1.2.1");
+  this.service.getCharacteristic(Characteristic.On)
+    .on('set', this.setPowerState.bind(this));
 
   // use gpio pin numbering
   rpio.init({
     mapping: 'gpio'
   });
   rpio.open(this.pin, rpio.OUTPUT, this.invert ? rpio.LOW : rpio.HIGH);
-  value = this.storage.getItemSync(stateKey);
-  this.log("Persisted state: " + value);
-  this.setPowerState(value, e => {});
-  this.service.getCharacteristic(Characteristic.On).updateValue(value);
-
-  this.service
-    .getCharacteristic(Characteristic.On)
-    .on('set', this.setPowerState.bind(this));
+  this.storage.init({dir: cacheDir, forgiveParseErrors: true}).then(
+    () => {
+      return this.storage.getItem(stateKey);
+    }).then(
+      value => {
+        this.log("Persisted state: " + value);
+        this.setPowerState(value, e => {});
+        this.service.getCharacteristic(Characteristic.On).updateValue(value);
+    });
 }
 
 SwitchAccessory.prototype.setPowerState = function(value, callback) {
   this.log("Setting switch to %s", value);
   if (this.invert) value = !value;
   rpio.write(this.pin, (value ? rpio.LOW : rpio.HIGH));
-  this.storage.setItemSync(stateKey, value);
-  callback();
+  this.storage.setItem(stateKey, value).then(() => {callback();});
 }
 
 SwitchAccessory.prototype.getServices = function() {
